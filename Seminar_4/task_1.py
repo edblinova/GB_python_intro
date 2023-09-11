15af5f23655d93dbbfcd51daa9f565bafa40f7d6
edblinova <greatner@yandex.ru>
2023-09-11 10:58:16 +0500

    add_task4

---
+++ Seminar_4/task_1.py
@@ -0,0 +1,50 @@
+# Даны два неупорядоченных набора целых чисел (может быть, с повторениями). 
+# Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+
+
+# Пользователь вводит 2 числа. 
+# n — кол-во элементов первого множества. 
+# m — кол-во элементов второго множества. 
+# Затем пользователь вводит сами элементы множеств.
+
+
+import logging
+from helpers.check_input import input_pos_int, input_int
+
+
+def main():
+
+    n_set = set()
+    m_set = set()
+
+    n = -1
+    while n == -1:
+        n = input_pos_int('элементов первого множества')
+    
+    m = -1
+    while m == -1:
+        m = input_pos_int('элементов второго множества')
+
+    for _ in range(n):
+        
+        new_el = 0.5
+        while new_el == 0.5:
+            new_el = input_int('элемент первого множества')
+        
+        n_set.add(new_el)
+
+    
+    for _ in range(m):
+        
+        new_el = 0.5
+        while new_el == 0.5:
+            new_el = input_int('элемент второго множества')
+        
+        m_set.add(new_el)
+    
+    result = sorted(n_set.intersection(m_set))
+    logging.warning(f'Результат: {result}')
+
+
+if __name__ == '__main__':
+    main()
